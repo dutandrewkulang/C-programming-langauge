52f19122ffb77fff9a1b758baf0f8a956b83eff7
Dukula <dutandrew78@gmail.com>
2022-10-08 15:24:06 +0300

    learning C for fun

--- story.c
+++ story.c
@@ -3,8 +3,11 @@
 void main(void)
 {
 	int num1, year, num2;
+	char name[] = "Dut Andrew Kulang", course[] = "Software Engineering",
+	language = 'C', sex[] = "Male";
 	num1 = 2022;
 	num2 = 2000;
 	year = num1 - num2;
-	printf("%d\n", year);
+	printf("Am %s a %d year old %s %s student learning %c for fun at ALX \n",
+	name, year, sex, course, language);
 }
